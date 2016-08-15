d89180de31a514c39eaf202a826c9569c2998eb8
homam <bluesnowball@gmail.com>
2016-08-16 01:21:48 +0200

    completed updated model in hlib

--- src/HLib.hs
+++ src/HLib.hs
@@ -1,16 +1,27 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts #-}
 module HLib
+(
+  Node(..), Conversation(..), SerializableUser(..),
+  processSerializedUserMessageAndUpdateUser
+)
 where
 
 import qualified Data.Map as M
 import Control.Monad.Trans
+import Control.Arrow ((***))
 type Question = String
-data ErrorType =  InvalidInputError | EdgeNotFoundError
+data ErrorType =  InvalidInputError | EdgeNotFoundError | ConversationNotFound | NodeNotFound
 
 
 toEither :: a -> Maybe b -> Either a b
 toEither _ (Just b) = Right b
 toEither a _ = Left a
+
+mapMTuples :: (Monad m) => (a -> m b) -> (c -> m d) -> [(a, c)] -> m [(b, d)]
+mapMTuples f g = mapM (sequenceTuple . (f *** g)) where
+  sequenceTuple :: (Monad m) => (m a, m b) -> m (a, b)
+  sequenceTuple (ma, mb) = (,) <$> ma <*> mb
+
 
 -- Node
 -- @nid@ :: NodeId
@@ -62,22 +73,28 @@ updateUser user (ustate', ConversationTarget nconv) = user {
 processMessageAndUpdateUser :: (Ord nid) => User cid nid us -> String -> Either ErrorType (User cid nid us)
 processMessageAndUpdateUser user input = updateUser user <$> processMessage user input
 
-
-
-
 data SerializableUser cid nid us = SerializableUser {
   serializableConversations :: [(cid, nid)],
   serializableUserState :: us
 }
 
-processSerializedUserMessageAndUpdateUser :: (Monad f, Monad m, Ord nid) =>
-   (suser -> m (f (User cid nid us))) ->
-   suser -> String -> m (f (Either ErrorType (User cid nid us)))
-processSerializedUserMessageAndUpdateUser userDeserializer suser input = do
-  euser <- userDeserializer suser
-  return $ (`processMessageAndUpdateUser` input) <$> euser
-
-
--- needs: (a -> m b) -> (c -> m d) -> [(a, c)] -> m [(b, d)]
 defaultUserDeserializer :: (Monad m) => (cid -> m (Conversation cid nid us)) -> (nid -> m (Node nid us)) -> SerializableUser cid nid us -> m (User cid nid us)
-defaultUserDeserializer conversationGetter nodeGetter suser = undefined
+defaultUserDeserializer conversationGetter nodeGetter suser = do
+  convs <- mapMTuples conversationGetter nodeGetter (serializableConversations suser)
+  return $ User convs (serializableUserState suser)
+
+type UserId = String
+
+-- main export
+processSerializedUserMessageAndUpdateUser :: (Monad m, Ord nid) =>
+  (cid -> Maybe (Conversation cid nid us)) ->
+  (nid -> Maybe (Node nid us)) ->
+  (UserId -> m (Either ErrorType (SerializableUser cid nid us))) ->
+  UserId -> String -> m (Either ErrorType (User cid nid us))
+processSerializedUserMessageAndUpdateUser conversationGetter nodeGetter userGetter userId input =
+  processEitherUserMessageAndUpdateUser <$> userGetter userId where
+
+    processEitherUserMessageAndUpdateUser fuser = do
+      suser <- fuser
+      user <- defaultUserDeserializer (toEither ConversationNotFound . conversationGetter) (toEither NodeNotFound . nodeGetter) suser
+      processMessageAndUpdateUser user input
